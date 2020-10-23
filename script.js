4a8ab69c258f5c116c37fd9d5ed4b9e5f1b1c09d
Tyler Hawkins <tylerhawkins@workfront.com>
2020-10-23 14:05:30 -0600

    feat: uses print styles and excludes non-table content from PDF

--- script.js
+++ script.js
@@ -2,12 +2,8 @@ var downloadPDF = function() {
   DocRaptor.createAndDownloadDoc("YOUR_API_KEY_HERE", {
     test: true, // test documents are free, but watermarked
     type: "pdf",
-    document_content: document.querySelector('html').innerHTML, // use this page's HTML
-    // document_content: "<h1>Hello world!</h1>",               // or supply HTML directly
-    // document_url: "http://example.com/your-page",            // or use a URL
-    // javascript: true,                                        // enable JavaScript processing
+    document_content: document.querySelector('html').innerHTML,
     prince_options: {
-      media: "screen",                                       // use screen styles instead of print styles
       baseurl: "tylerhawkins.info/html-to-pdf-demo",
     }
   })
